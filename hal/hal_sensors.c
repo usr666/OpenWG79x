5fc6fe48e56756d6f3a5bc3420dd3547dd469c71
Magnus <magnus@fastadress.se>
2025-06-01 19:12:48 +0200

    Fixed wire sensor time measure debug menu

--- hal/hal_sensors.c
+++ hal/hal_sensors.c
@@ -44,15 +44,29 @@ bool get_sensor(sensors_t sensor)
 
 interruptdata_t interruptdata[NO_OF_INTERRUPTDATA];
 static uint8_t index = 0;
+static bool firstrun;
 
 void EnableTimeMeasure(void)
 {
     index = 0;
+    firstrun = true;
     LPC_GPIOINT->IO0IntEnR = 0x280;
     LPC_GPIOINT->IO0IntEnF = 0x500;
     NVIC_EnableIRQ(EINT3_IRQn);    
 }
 
+#define RIGHT_SENSOR_RISING_EDGE_BITVAL 0x200
+#define LEFT_SENSOR_RISING_EDGE_BITVAL 0x080
+#define RIGHT_SENSOR_FALLING_EDGE_BITVAL 0x400
+#define LEFT_SENSOR_FALLING_EDGE_BITVAL 0x100
+/* Conclusions of test with wire sensors
+     If the first edge after a long time without edges (at least 0x10000 TIMER0-ticks) is a rising edge that sensor is inside the wire
+     If the first edge after a long time without edges (at least 0x10000 TIMER0-ticks) is a falling edge that sensor is outside the wire
+     Bit 0x200 is Right sensor rising edge
+     Bit 0x400 is Right sensor falling edge
+     Bit 0x080 is Left sensor rising edge
+     Bit 0x100 is Left sensor falling edge
+*/
 void __attribute__ ((interrupt)) EINT3_IRQHandler(void)
 {
     uint32_t time;
@@ -68,6 +82,11 @@ void __attribute__ ((interrupt)) EINT3_IRQHandler(void)
     intf = LPC_GPIOINT->IO0IntStatF;
     LPC_GPIOINT->IO0IntClr = intr;
     LPC_GPIOINT->IO0IntClr = intf;
+
+    if(firstrun) {
+        firstrun = false;
+        return;
+    }
 
     if(index < NO_OF_INTERRUPTDATA) {
         if(index>0 || time > 100000) {
